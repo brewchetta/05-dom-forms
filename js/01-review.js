77d1a0ca7fc3002adbcc73bf6837ff3de6d40537
Dan Rodney <chett.tiller@gmail.com>
2026-02-09 11:11:47 -0500

    shark week

--- js/01-review.js
+++ js/01-review.js
@@ -1,6 +1,6 @@
 // Review
 
-// 1. Choose one of the sharks in the "sharksArray" as your featured shark. Accessing that shark, change each section of the "featured-shark" section so that it has the details for that shark. Do this using Javascript DOM manipulation.
+// 1. Choose one of the sharks in the `sharksArray` as your featured shark. Accessing that shark, change each section of the "featured-shark" section so that it has the details for that shark including <li> elements for the "featured-diet" and "featured-habitats" sections. Do this using Javascript DOM manipulation.
 
 // 2. For each shark object in the sharksArray append a new <li> in the "shark-facts-list" with that shark's `funFact`. Do this using Javascript DOM manipulation.
 
@@ -23,6 +23,51 @@ const sharksArray = [
         name: "Nurse Shark",
         species: "ginglymostoma cirratum",
         diet: ["small fish", "crustaceans", "molluscs", "algae"],
-        funFact: `The name "nurse" may have originated from antiquated spelling conventions. The Oxford English Dictionary notes that, in medieval times, the "n" of the word "an" was frequently transferred to a following word that began with a vowel. Huss, husse and hurse were antiquated names for dogfish and other sharks.`,habitats: ["Eastern Atlantic", "Western Atlantic", "East Pacific"],
+        funFact: `The name "nurse" may have originated from antiquated spelling conventions. The Oxford English Dictionary notes that, in medieval times, the "n" of the word "an" was frequently transferred to a following word that began with a vowel. Huss, husse and hurse were antiquated names for dogfish and other sharks.`,
+        habitats: ["Eastern Atlantic", "Western Atlantic", "East Pacific"],
     },
 ]
+
+const featuredShark = sharksArray[2]
+
+const featuredName = document.querySelector("#featured-name")
+const featuredSpecies = document.querySelector("#featured-species")
+const featuredFact = document.querySelector("#featured-fact")
+const featuredDiet = document.querySelector("#featured-diet")
+const featuredHabitats = document.querySelector("#featured-habitats")
+
+featuredName.textContent = featuredShark.name
+featuredSpecies.textContent = featuredShark.species
+featuredFact.textContent = featuredShark.funFact
+
+// ADD DIET FOR FEATURED SHARK
+
+function createDietLi(dietItem) {
+    const newLi = document.createElement("li")
+    newLi.textContent = dietItem
+    featuredDiet.append(newLi)
+}
+
+featuredShark.diet.forEach( dietItem => createDietLi(dietItem) )
+
+// ADD HABITATS FOR FEATURED SHARK
+
+function createHabitatLi(habitatItem) {
+    const newLi = document.createElement("li")
+    newLi.textContent = habitatItem
+    featuredHabitats.append(newLi)
+}
+
+featuredShark.habitats.forEach( habitatItem => createHabitatLi(habitatItem) )
+
+// SHARK FACTS UL
+
+const sharkFactsList = document.querySelector("#shark-facts-list")
+
+function createFactLi( sharkFact ) {
+    const newLi = document.createElement("li")
+    newLi.textContent = sharkFact
+    sharkFactsList.append( newLi )
+}
+
+sharksArray.forEach( shark => createFactLi( shark.funFact ) )
