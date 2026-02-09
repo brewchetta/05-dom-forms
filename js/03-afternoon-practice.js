1c6d1b1a18ef69cee434dd6de0bd10900a062d88
Dan Rodney <chett.tiller@gmail.com>
2026-02-09 14:23:13 -0500

    bonus deliverables

--- js/03-afternoon-practice.js
+++ js/03-afternoon-practice.js
@@ -1,7 +1,17 @@
-// Mini project: create a todo list page. This has two main sections: a <form> and an <ol>. You should do this directly in the HTML.
+// MAIN DELIVERABLES //
 
-// The form has three inputs: a text input for the name of the task, a number input for the priority of the task, and a submit. 
+// Mini project: create a todo list page. This has two main sections: a <form> and an <ol>. You should add these directly in the HTML.
+
+// The form has three inputs: a text input for the name of the task, a number input for the priority of the task, and a submit input. 
 
 // As you create the <li> also create a delete <button> and append it to the <li>. Add an event listener to the <button> where when it's clicked, the <li> will `.remove()` from the page. Be mindful of scope... there are multiple ways to target the  <li> and some are easier than others!
 
 // Add a "delete-all" <button> above the list (you may do this directly in the html). When the button has been clicked, delete all items in the <ol>.
+
+
+// BONUS DELIVERABLES //
+
+// The number for the task priority changes the color of the <li> text. For example, maybe priority 1 causes it to be red, priority 2 makes it yellow, and priority 3 makes it green (you choose the colors and what numbers they correspond to!)
+
+// Make an additional <ul> for completed tasks below the <ol> for completed tasks. When the delete <button> is clicked the button is removed and the task is moved to the <ul> for completed tasks.
+// HINT: You may .append an element again to change where it lives...
